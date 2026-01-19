19d5d9a3919ba1072b29e1ce8b947876430b78cc
Naren456 <narendrase666@gmail.com>
2026-01-19 21:26:31 +0530

    fix code

--- src/main/java/com/shopping/Ecommerce/model/Product.java
+++ src/main/java/com/shopping/Ecommerce/model/Product.java
@@ -3,22 +3,15 @@ package com.shopping.Ecommerce.model;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "products")
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
-    private Double price;
-    private Integer stock;
+    private double price;
 
-    public Product() {}
-
-    public Product(Long id, String name, Double price, Integer stock) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.stock = stock;
-    }
 
     public Long getId() {
         return id;
@@ -36,19 +29,11 @@ public class Product {
         this.name = name;
     }
 
-    public Double getPrice() {
+    public double getPrice() {
         return price;
     }
 
-    public void setPrice(Double price) {
+    public void setPrice(double price) {
         this.price = price;
-    }
-
-    public Integer getStock() {
-        return stock;
-    }
-
-    public void setStock(Integer stock) {
-        this.stock = stock;
     }
 }
